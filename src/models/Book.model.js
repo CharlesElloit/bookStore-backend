c9ae86db84a3055d2912319f393c2be832487fd5
Charles Elloit <aciforcharles@gmail.com>
2021-02-04 14:31:28 -0500

    Added some new field on the model

--- src/models/Book.model.js
+++ src/models/Book.model.js
@@ -3,13 +3,29 @@ const mongoose = require("mongoose");
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true },
   subtitle: { type: String, trim: true },
-  description: { type: String, required: true, trim: true },
-  bookEdition: { type: String, required: true, trim: true },
-  bookCover: { type: String, required: true },
-  author: { type: String, required: true },
+  authors: [{ type: String, required: true, trim: true }],
   publisher: { type: String, required: true, trim: true },
-  published_Date: { type: Date, required: true },
-  ISBN: { type: String }
+  publishedDate: { type: Date, required: true },
+  description: { type: String, trim: true },
+  industryIdentifiers: { type: { type: String }, identifier: String },
+  pageCount: { type: Number, required: true },
+  categories: [{ type: String, required: true }],
+  bookCoverImageLinks: {
+    thumbnail: { type: String },
+    small: { type: String },
+    medium: { type: String },
+    large: { type: String }
+  },
+  bookEdition: { type: String, required: true, trim: true },
+  language: { type: String, required: true },
+  epub: {
+    isAvailable: { type: Boolean, required: true },
+    downloadLink: { type: String }
+  },
+  pdf: {
+    isAvailable: { type: Boolean, required: true },
+    downloadLink: { type: String }
+  }
 });
 
 module.exports = mongoose.model("Book", bookSchema);
